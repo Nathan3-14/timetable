cde54d06bfbeb9099ebb459a4574926c94e5d695
Ayaan <ayaan.waqas@outlook.com>
2025-12-10 12:41:29 +0000

    LESS POSITIONING WORKS

--- dioxus_timetable/src/main.rs
+++ dioxus_timetable/src/main.rs
@@ -5,39 +5,6 @@ use crate::components::*;
 use crate::pages::*;
 use dioxus::prelude::*;
 
-#[derive(Clone, PartialEq)]
-pub struct Lesson {
-    subject: String,
-    teacher_name: String,
-    time: String,
-    room: String,
-}
-
-#[component]
-pub fn LessonEl(lesson: Lesson) -> Element {
-    let times: Vec<String> = lesson
-        .time
-        .split("-")
-        .map(|time| time.to_string())
-        .collect();
-
-    if times.len() != 2 {
-        return rsx! {};
-    }
-
-    let start_times: Vec<String> = times[0].split(":").map(|x| x.to_string()).collect();
-    let end_times: Vec<String> = times[1].split(":").map(|x| x.to_string()).collect();
-
-    rsx! {
-        div { id: "lesson", background_color: "var(--red)",
-            h1 { id: "lesson__subject-name", "{lesson.subject}" }
-            p { id: "lesson__teacher-name", "{lesson.teacher_name}" }
-            p { id: "lesson__room", "{lesson.room}" }
-            p { id: "lesson__time", "from {start_times:#?} to {end_times:#?}" }
-        }
-    }
-}
-
 #[derive(Routable, Clone, PartialEq)]
 enum Route {
     #[layout(Navbar)]
--- dioxus_timetable/src/pages/timetable.rs
+++ dioxus_timetable/src/pages/timetable.rs
@@ -1,6 +1,54 @@
 use dioxus::prelude::*;
 
-use crate::{Lesson, LessonEl};
+#[derive(Clone, PartialEq)]
+pub struct Lesson {
+    subject: String,
+    teacher_name: String,
+    time: String,
+    room: String,
+}
+
+#[component]
+pub fn LessonEl(lesson: Lesson) -> Element {
+    let times: Vec<String> = lesson
+        .time
+        .split("-")
+        .map(|time| time.to_string())
+        .collect();
+
+    if times.len() != 2 {
+        return rsx! {};
+    }
+
+    let start_times: Vec<String> = times[0].split(":").map(|x| x.to_string()).collect();
+    let start_hour: i8 = start_times[0].parse::<i8>()?;
+    let start_minute: i8 = start_times[1].parse::<i8>()?;
+
+    let mut grid_start: i8 = 1;
+    grid_start += (start_hour - 8) * 12; // 12 rows per hour
+    grid_start += start_minute / 5; // every 5 minutes is one row
+
+    let end_times: Vec<String> = times[1].split(":").map(|x| x.to_string()).collect();
+    let end_hour: i8 = end_times[0].parse::<i8>()?;
+    let end_minute: i8 = end_times[1].parse::<i8>()?;
+
+    let mut grid_span: i8 = 0;
+    grid_span += (end_hour - start_hour) * 12;
+    grid_span += (end_minute - start_minute) / 5;
+
+    rsx! {
+        div {
+            id: "lesson",
+            background_color: "var(--mauve)",
+            grid_row_start: grid_start,
+            grid_row_end: "span {grid_span}",
+            h1 { id: "lesson__subject-name", "{lesson.subject}" }
+            p { id: "lesson__teacher-name", "{lesson.teacher_name}" }
+            p { id: "lesson__room", "{lesson.room} {start_hour} {start_minute} {grid_start}" }
+            p { id: "lesson__time", "from {start_times:#?} to {end_times:#?}" }
+        }
+    }
+}
 
 #[component]
 pub fn Timetable() -> Element {
@@ -9,7 +57,7 @@ pub fn Timetable() -> Element {
     let example_lesson: Lesson = Lesson {
         subject: "subject".to_string(),
         teacher_name: "teacher name".to_string(),
-        time: "9:00-10:30".to_string(),
+        time: "9:30-11:30".to_string(),
         room: "room".to_string(),
     };
 
